4b655a9d6d948e245db6a3efcb4ce0c64093900b
Wassim Chegham <github@wassim.dev>
2019-09-01 20:31:29 +0200

    chore: add more types

--- type.d.ts
+++ type.d.ts
@@ -1,5 +1,22 @@
-declare interface AzureSubscription {
-  id: "string";
-  name: "string";
+declare interface AzureEntity {
+  id: string;
+  name: string;
+}
+
+declare interface AzureRegion extends AzureEntity {
+  displayName: string;
+}
+
+declare interface AzureSubscription extends AzureEntity {
   state: "Enabled";
+}
+
+declare interface AzureResourceGroup extends AzureEntity {
+  location: string;
+}
+
+declare interface AzureStorage extends AzureEntity {}
+
+declare interface CommandOptions {
+  silent: boolean;
 }
