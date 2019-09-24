0b47620d16cbe5be7c9d926ab9396475130fd8bd
Wassim Chegham <github@wassim.dev>
2019-09-24 12:57:23 +0200

    feat: add hosting feature

--- src/features/hosting/command.ts
+++ src/features/hosting/command.ts
@@ -1,9 +1,7 @@
 import { az } from "../../core/utils";
 
 export async function push({ storageAccountName }: { storageAccountName: string }) {
-  await az(
-    `storage blob service-properties update --account-name ${storageAccountName} --static-website --404-document 404.html --index-document index.html`
-  );
+  
   // await az(
   //   `storage blob upload-batch -s <source-path> -d \$web --account-name <storage-account-name>`
   // );
--- src/features/hosting/index.ts
+++ src/features/hosting/index.ts
@@ -1,24 +1,38 @@
-import { QuestionCollection } from "inquirer";
-import { createDirectoryIfNotExists } from "../../core/utils";
 import inquirer = require("inquirer");
+import { askForHostingFolder } from "../../core/prompt";
+import { az, Config, copyTemplate, saveWorkspace } from "../../core/utils";
+import chalk from "chalk";
+const debug = require("debug")("hosting");
 
-// Note: use commonJs exports
-module.exports = async function(): Promise<inquirer.Answers> {
-  const questions: QuestionCollection = [
-    {
-      type: "input",
-      name: "folder",
-      message: "Enter public folder (will be created if not present):",
-      default: "public",
-      validate: function(value: string) {
-        if (value && value.length) {
-          // TODO: copy template files if new created folder
-          return createDirectoryIfNotExists(value);
-        } else {
-          return "Please enter a public folder.";
-        }
-      }
+module.exports = async function() {
+  let { folder, overrideHtml, override404, overrideError } = await askForHostingFolder();
+  debug(
+    `selected hosting folder=${folder}, overrideHtml=${overrideHtml}, override404=${override404}. overrideError=${overrideError}`
+  );
+
+  if (overrideHtml || typeof overrideHtml === "undefined") {
+    // copy index.html
+    copyTemplate(`init/index.html`, `${folder}/index.html`);
+  }
+  if (override404 || typeof override404 === "undefined") {
+    // copy 404.html
+    copyTemplate(`init/404.html`, `${folder}/404.html`);
+  }
+  if (overrideError || typeof overrideError === "undefined") {
+    // copy 404.html
+    copyTemplate(`init/error.html`, `${folder}/error.html`);
+  }
+
+  const storage: AzureStorage = Config.get("storage");
+  debug(`using storage ${chalk.green(JSON.stringify(storage))}`);
+
+  await az(
+    `storage blob service-properties update --account-name ${storage.name} --static-website --404-document 404.html --index-document index.html`
+  );
+
+  saveWorkspace({
+    hosting: {
+      public: folder
     }
-  ];
-  return inquirer.prompt(questions);
+  });
 };
