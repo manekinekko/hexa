55ea5eb28878349f296d722f55d2e8779eacf176
Wassim Chegham <github@wassim.dev>
2019-09-24 13:54:27 +0200

    feat: add hosting creation and cors config

--- src/features/hosting/index.ts
+++ src/features/hosting/index.ts
@@ -26,8 +26,21 @@ module.exports = async function() {
   const storage: AzureStorage = Config.get("storage");
   debug(`using storage ${chalk.green(JSON.stringify(storage))}`);
 
+  const subscription: AzureSubscription = Config.get("subscription");
+  debug(`using subscription ${chalk.green(JSON.stringify(subscription))}`);
+
+  // https://docs.microsoft.com/en-us/cli/azure/storage/blob/service-properties?view=azure-cli-latest#az-storage-blob-service-properties-update
   await az(
-    `storage blob service-properties update --account-name ${storage.name} --static-website --404-document 404.html --index-document index.html`
+    `storage blob service-properties update --account-name "${storage.name}" --static-website --404-document 404.html --index-document index.html --query '{staticWebsite: staticWebsite}'`,
+    `Enabling hosting service...`
+    );
+    
+    // https://docs.microsoft.com/en-us/cli/azure/storage/cors?view=azure-cli-latest#az-storage-cors-clear
+    await az(`storage cors clear --services "b" --account-name "${storage.name}" --subscription "${subscription.id}"`);
+    // https://docs.microsoft.com/en-us/cli/azure/storage/cors?view=azure-cli-latest#az-storage-cors-add
+    await az(
+      `storage cors add --methods GET HEAD MERGE OPTIONS POST PUT --origins "*" --services "b" --account-name "${storage.name}" --subscription "${subscription.id}" --max-age 3600`,
+      `Enabling CORS...`
   );
 
   saveWorkspace({
