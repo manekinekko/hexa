cbacdabd04acabc69a1f3addfb5ffc1a4cfeae01
Wassim Chegham <github@wassim.dev>
2019-09-24 17:05:37 +0200

    chore: add NitroWorkspace type

--- type.d.ts
+++ type.d.ts
@@ -1,3 +1,11 @@
+declare interface NitroWorkspace {
+  project: string;
+  storage: AzureStorage;
+  hosting: {
+    public: string;
+  };
+  ressourceGroupe: AzureResourceGroup;
+}
 declare interface AzureEntity {
   id: string & CreationMode;
   name: string;
@@ -16,7 +24,10 @@ declare interface AzureResourceGroup extends AzureEntity {
   location: string;
 }
 
-declare interface AzureStorage extends AzureEntity {}
+declare interface AzureStorage extends AzureEntity {
+  sas?: string;
+  connectionString?: string;
+}
 declare interface AzureStorageToken extends AzureStorage {
   sas?: string;
   connectionString?: string;
