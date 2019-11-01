755ef5f4ef7ce04b3448896e1e22623c92aface4
Wassim Chegham <github@wassim.dev>
2019-11-01 13:33:08 +0100

    chore: improve resource group creation

--- src/features/resource-group/create.ts
+++ src/features/resource-group/create.ts
@@ -15,21 +15,27 @@ module.exports = async function(creationMode: CreationMode) {
   debug(`saving project ${name}`);
 
   let region = "westeurope";
+  let isProjectExists = false;
+  let project: AzureResourceGroup;
 
   if (creationMode === "MANUAL") {
     // https://docs.microsoft.com/en-us/cli/azure/account?view=azure-cli-latest#az-account-list-locations
-    let regionsList = await az<AzureRegion[]>(
-      `account list-locations --query "[].{name:name, id:id, displayName:displayName}"`,
-      `Loading Azure regions (this may take few minutes)...`
-    );
+    let regionsList = await az<AzureRegion[]>(`account list-locations --query "[].{name:name, id:id, displayName:displayName}"`, `Loading regions (this may take few minutes)...`);
     ({ name, region } = await askForResourceGroupDetails(regionsList, name, region));
+  } else {
+    isProjectExists = await az<boolean>(`group exists --name ${name}`, `Checking for existing project ${chalk.cyan(name)}...`);
   }
 
-  // https://docs.microsoft.com/en-us/cli/azure/group?view=azure-cli-latest#az-group-create
-  let project = await az<AzureResourceGroup>(
-    `group create -l ${region} -n ${name} --tag "x-created-by=hexa" --query "{name:name, id:id, location:location}"`,
-    `Bootstrapping project ${chalk.cyan(name)}...`
-  );
+  if (isProjectExists) {
+    // https://docs.microsoft.com/en-us/cli/azure/group?view=azure-cli-latest#az-group-show
+    project = await az<AzureResourceGroup>(`group show -n workspacetest --query "{name:name, id:id, location:location}"`, `Bootstrapping project ${chalk.cyan(name)}...`);
+  } else {
+    // https://docs.microsoft.com/en-us/cli/azure/group?view=azure-cli-latest#az-group-create
+    project = await az<AzureResourceGroup>(
+      `group create -l ${region} -n ${name} --tag "x-created-by=hexa" --query "{name:name, id:id, location:location}"`,
+      `Bootstrapping project ${chalk.cyan(name)}...`
+    );
+  }
 
   Config.set("project", project);
 
